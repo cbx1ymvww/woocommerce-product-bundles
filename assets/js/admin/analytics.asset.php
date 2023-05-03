dcf551b11fd0bdeeab9486235475819e17755847
cbx.dev <132402110+cbx1ymvww@users.noreply.github.com>
2023-05-03 11:37:23 +0300

    Add files via upload

---
+++ assets/js/admin/analytics.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wc-components', 'wc-csv', 'wc-currency', 'wc-date', 'wc-navigation', 'wc-number', 'wc-settings', 'wc-store-data', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-dom', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => 'c510811100b1cc266a3a20f2b02c555d');
